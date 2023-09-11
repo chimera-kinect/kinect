663e089930b7f8660c4704da6e5ef5243712c975
Alsje <alsje@mernok.co.za>
2023-09-11 16:04:14 +0200

    fix: rndInShape had IndexError

--- utilities/record.py
+++ utilities/record.py
@@ -39,7 +39,7 @@ def main():
     for _ in range(framesN):
         capture = k4a.get_capture()
         if np.any(capture.depth):
-            # don't feel like making the clipping range a user input rn, sorry ¯\_(ツ)_/¯
+            # don't feel like making the clipping range a user input rn, sorry ¯\_(ツ)_/¯ (Love, Bozo)
             frames.append(grayscale(capture.depth, (300, 500)))
             
     k4a.stop()
@@ -49,7 +49,7 @@ def main():
     # make sure everything was saved correctly
     loaded_frames = np.load(filePath)
     rndFrame = random.randint(0, framesN - 1)
-    rndInShape = random.randint(frames[0][0].shape[0])
+    rndInShape = random.randint(frames[0][0].shape[0], frames[0][0].shape[0])-1
     assert frames[rndFrame].shape == loaded_frames[rndFrame].shape
     assert frames[rndFrame][rndInShape][rndInShape] == loaded_frames[rndFrame][rndInShape][rndInShape]
 
