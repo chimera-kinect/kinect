a7af2e98f79e38e7bfe73292f35b7b2b7f4623c8
Alsje <alsje@mernok.co.za>
2023-10-05 17:12:03 +0200

    feat/crop-image finished normalised clipping rage

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import cv2
 import numpy as np
 
-from typing import Optional, Tuple
-
 import pyk4a
 from pyk4a import Config, PyK4A
 
-def grayscale(
+def clip_and_crop(
     image: np.ndarray,
-    clipping_range: Tuple[Optional[int], Optional[int]] = (None, None)
 ) -> np.ndarray:
-    if clipping_range[0] or clipping_range[1]:
-        img = image.clip(clipping_range[0], clipping_range[1])  # clamps values between min and max
-    else:
-        img = image.copy()
-    
-    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U) #grayscale
-    return img
+
+    return image[y1:y2, x1:x2].clip(500, 1100) # clamps values between min and max
 
 def colorize(
     image: np.ndarray,
-    clipping_range: Tuple[Optional[int], Optional[int]] = (None, None),
     colormap: int = cv2.COLORMAP_HOT,
 ) -> np.ndarray:
-    if clipping_range[0] or clipping_range[1]:
-        img = image.clip(clipping_range[0], clipping_range[1])  # clamps values between min and max
-    else:
-        img = image.copy()
-    
-    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U) #grayscale
-    img = cv2.applyColorMap(img, colormap)
-    return img
+
+    img = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U) #grayscale
+    return cv2.applyColorMap(img, colormap)
+
+x1, y1 = 70, 47
+x2, y2 = 1072, 705
+push_threshold = 100
+calibration_depth = None
+
+def test(
+    image: np.ndarray,
+) -> np.ndarray:
+    normalized_image = (image - 500) / (calibration_depth - 500)
+    # OUR PAST PROBLEM
+    normalized_image = 1 - np.clip(normalized_image, 0, 1)
+    return (normalized_image * 255).astype(np.uint8)
 
 def main():
+    global calibration_depth
+
     k4a = PyK4A(
         Config(
             color_resolution=pyk4a.ColorResolution.RES_720P,
@@ -43,13 +44,20 @@ def main():
     )
 
     k4a.start()
-
     capture = k4a.get_capture()
     if np.any(capture.transformed_depth):
-        img = colorize(capture.transformed_depth, (None, None))
-        # correct crop
-        cv2.imshow("k4a", cv2.rectangle(img, (70, 47), (1072, 705), (255, 0, 0)))
-        key = cv2.waitKey(5000)
+        calibration_depth = clip_and_crop(capture.transformed_depth)
+
+    while True:
+        capture = k4a.get_capture()
+        if np.any(capture.transformed_depth):
+            depth_frame = clip_and_crop(capture.transformed_depth)
+            cv2.imshow('k4a', test(depth_frame))
+
+            key = cv2.waitKey(10)
+            if key != -1:
+                cv2.destroyAllWindows()
+                break
 
     k4a.stop()
 
